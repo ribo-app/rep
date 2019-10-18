33ae97d36511f53187a8c67de1f30a66c24a405f
eugeniaortega55 <eugenia.ortega05@gmail.com>
2019-10-18 13:09:18 +0200

    Jupyter con los resultados de todos los paises y funcion

--- financial_reports.py
+++ financial_reports.py
@@ -1,20 +1,39 @@
-import pandas as pd 
-
+import pandas as pd
+import datetime
 
 def p_and_l_results(file_name):
     df = pd.read_csv(file_name)
+    df.columns = ['month', 'year', 'total_fee', 'total_cost', 'total_management_interest', 'total_management_fee']
+    df['day'] = 1
+    
+    date = pd.to_datetime(df[['year', 'month', 'day']])
+    df = df.drop(['month', 'year', 'day'], axis=1)
+    df['date'] = date
+    df.set_index('date', inplace=True, drop=True)
+    df = df.sort_index()
+    df.sort_index(inplace=True)
+
+    if file_name == "resultadosDR.csv":
+        df = df.div(52)
+
     total_ingresos = df['total_fee'] + df['total_management_interest'] + df['total_management_fee']
     df['total_income'] = total_ingresos
     utilidad = df['total_income'] - df['total_cost']
-    df['utilidad'] = utilidad 
+    df['utilidad'] = utilidad
+
+    
 
     cols= df.columns.tolist()
     column_to_move = 'total_cost'
-    new_position = 6
+    new_position = 4
 
 
-    cols.insert(new_position, cols.pop(cols.index(column_to_move)) )
+    cols.insert(new_position, cols.pop(cols.index(column_to_move)))
     df= df[cols]
 
-    return df.style.hide_index()
+
+    return df
+
+
+
 
